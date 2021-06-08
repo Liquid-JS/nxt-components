a37d482fa840831a612966d2a0a78c712c30a540
HitkoDev <development@hitko.si>
2021-06-08 15:14:20 +0200

    feat: improve demo, add build scripts

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core'
+import { remove as removeDiacritics } from 'diacritics'
+import * as countries from 'i18n-iso-countries'
+import locl from 'i18n-iso-countries/langs/en.json'
+import { FlagDatabase } from 'nxt-flags'
+
+countries.registerLocale(locl)
 
 @Component({
     selector: 'app-root',
@@ -7,4 +13,24 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
     code = 'es'
+    data: { code: string, name: string }[]
+
+    constructor() {
+        const codes = Object.keys(FlagDatabase).filter(v => v.length == 2)
+        this.data = codes.map(c => ({
+            code: c.toLowerCase(),
+            name: countries.getName(c.toUpperCase(), 'en')
+        })).filter(({ name }) => !!name)
+            .sort((a, b) => normalizeCompare(a.name, b.name))
+    }
+}
+
+function normalizeCompare(a: string, b: string) {
+    return normalize(a).localeCompare(normalize(b))
+}
+
+function normalize(val: string) {
+    return removeDiacritics('' + (val || ''))
+        .toLowerCase()
+        .trim()
 }
