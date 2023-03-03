f055e1e7846b9355acde8de6b50dc38c913a7b3e
HitkoDev <development@hitko.si>
2023-03-03 13:08:20 +0100

    chore: inital commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+        '@commitlint/config-conventional'
+    ],
+    plugins: [
+        'workspace-scopes'
+    ],
+    rules: {
+        'scope-enum': [2, 'always', []],
+    }
+}
