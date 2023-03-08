19d0e15fbbc74ce49cb32832fb191b1fea94c8ad
HitkoDev <development@hitko.si>
2023-03-08 10:43:22 +0100

    feat(demo): add service worker

--- packages/demo/src/app/app.module.ts
+++ packages/demo/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core'
+import { isDevMode, NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ServiceWorkerModule } from '@angular/service-worker'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
@@ -13,7 +14,13 @@ import { HomeComponent } from './home/home.component'
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
