4aa143c470599f818f38822fb339c23ace42b5a3
HitkoDev <development@hitko.si>
2023-05-05 13:12:16 +0200

    docs: gs verification

--- scripts/add-404.ts
+++ scripts/add-404.ts
@@ -9,3 +9,20 @@ readFile('docs/demo/index.html', 'utf8')
         await writeFile('docs/404.html', content, 'utf8')
     })
     .catch(console.error)
+
+readFile('resources/google9e159bb1235d6af3.html', 'utf8')
+    .then(async content => {
+        await writeFile('docs/google9e159bb1235d6af3.html', content, 'utf8')
+    })
+    .catch(console.error)
+
+readFile('docs/demo/ngsw.json', 'utf8')
+    .then(async content => {
+        const prefix = '/nxt-components/demo'
+        const c = JSON.parse(content)
+        c.index = prefix + c.index
+        c.hashTable = Object.fromEntries(Object.entries(c.hashTable).map(([k, v]) => [prefix + k, v]))
+        c.assetGroups.forEach((g: any) => g.urls = g.urls.map((u: any) => prefix + u))
+        await writeFile('docs/demo/ngsw.json', JSON.stringify(c, undefined, 2), 'utf8')
+    })
+    .catch(console.error)
