d78fb6d3e38a02495c1d6e9f17d47c27cdb4e87d
HitkoDev <development@hitko.si>
2024-07-06 20:51:52 +0200

    chore: upd

--- packages/demo/src/app/app.component.spec.ts
+++ packages/demo/src/app/app.component.spec.ts
@@ -1,13 +1,10 @@
 import { TestBed } from '@angular/core/testing'
-import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [
-                RouterTestingModule
-            ],
+            imports: [],
             declarations: [
                 AppComponent
             ]
