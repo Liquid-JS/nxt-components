efd9b9e61d8f0eef9bb6c3419ffe2eea27c61aaf
Hitko <development@hitko.si>
2025-01-27 14:55:30 +0100

    chore(highlightjs): cleanup

--- packages/highlightjs/src/test-helpers.ts
+++ packages/highlightjs/src/test-helpers.ts
@@ -2,14 +2,14 @@ import hljs, { type HLJSApi } from 'highlight.js';
 import { activateLineNumbers } from 'ngx-highlightjs/line-numbers';
 
 export async function afterTimeout(timeout: number): Promise<void> {
-  // Use await with a setTimeout promise
-  await new Promise<void>((resolve) => setTimeout(resolve, timeout));
+    // Use await with a setTimeout promise
+    await new Promise<void>((resolve) => setTimeout(resolve, timeout));
 }
 
 // Fake Highlight Loader
 export const highlightLoaderStub = {
-  ready: new Promise<HLJSApi>((resolve) => {
-    activateLineNumbers(hljs, document);
-    resolve(hljs);
-  })
+    ready: new Promise<HLJSApi>((resolve) => {
+        activateLineNumbers(hljs, document);
+        resolve(hljs);
+    })
 };
