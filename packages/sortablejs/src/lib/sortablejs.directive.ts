c103f89c2e25bce0ceb4c73570b3e0f6e944ea04
Hitko <development@hitko.si>
2025-12-23 20:33:18 +0100

    feat(sortablejs): use signals

--- packages/sortablejs/src/lib/sortablejs.directive.ts
+++ packages/sortablejs/src/lib/sortablejs.directive.ts
@@ -2,16 +2,15 @@ import {
     AfterViewInit,
     Directive,
     ElementRef,
-    EventEmitter,
-    Inject,
     NgZone,
     OnChanges,
     OnDestroy,
-    Optional,
-    Output,
     Renderer2,
     SimpleChange,
-    input
+    computed,
+    inject,
+    input,
+    output
 } from '@angular/core'
 import { type AbstractControl, type FormArray } from '@angular/forms'
 import Sortable, { Options, SortableEvent } from 'sortablejs'
@@ -24,6 +23,12 @@ export type CloneFunction<T> = (item: T) => T
 
 @Directive({ selector: '[nxtSortablejs]' })
 export class SortablejsDirective<T> implements OnChanges, OnDestroy, AfterViewInit {
+
+    private readonly globalConfig = inject(GLOBALS, { optional: true }) ?? undefined
+    private readonly service = inject(SortablejsService)
+    private readonly element = inject(ElementRef)
+    private readonly zone = inject(NgZone)
+    private readonly renderer = inject(Renderer2)
 
     /** Input data, can be Array or FormArray */
     readonly data = input<SortableData<T>>(undefined, { alias: 'nxtSortablejs' })
@@ -43,19 +48,69 @@ export class SortablejsDirective<T> implements OnChanges, OnDestroy, AfterViewIn
     readonly cloneFunction = input<CloneFunction<T>>()
 
     /** Initialised a new Sortablejs instance */
-    @Output() readonly init = new EventEmitter<Sortable>()
+    readonly init = output<Sortable>()
 
     private sortableInstance?: Sortable
 
-    constructor(
-        @Optional()
-        @Inject(GLOBALS)
-        private readonly globalConfig: Options | undefined,
-        private readonly service: SortablejsService,
-        private readonly element: ElementRef,
-        private readonly zone: NgZone,
-        private readonly renderer: Renderer2
-    ) { }
+    private readonly bindings = computed(() => {
+        const data = this.data()
+        if (!data) {
+            return new SortablejsBindings([])
+        } else if (data instanceof SortablejsBindings) {
+            return data
+        } else {
+            return new SortablejsBindings([data])
+        }
+    })
+
+    private readonly optionsWithoutEvents = computed(() => ({ ...(this.globalConfig || {}), ...(this.config() || {}) }))
+    private readonly overridenOptions = computed<Options>(() => ({
+        // always intercept standard events but act only in case items are set (bindingEnabled)
+        // allows to forget about tracking this.items changes
+        onAdd: (event: SortableEvent) => {
+            this.service.transfer = (items: any[]) => {
+                this.bindings().injectIntoEvery(event.newIndex!, items)
+                this.proxyEvent('onAdd', event)
+            }
+
+            this.proxyEvent('onAddOriginal', event)
+        },
+        onRemove: (event: SortableEvent) => {
+            const bindings = this.bindings()
+
+            if (bindings.provided) {
+                if (this.isCloning) {
+                    this.service.transfer?.(bindings.getFromEvery(event.oldIndex!).map(item => this.clone(item)))
+
+                    // great thanks to https://github.com/tauu
+                    // event.item is the original item from the source list which is moved to the target list
+                    // event.clone is a clone of the original item and will be added to source list
+                    // If bindings are provided, adding the item dom element to the target list causes artifacts
+                    // as it interferes with the rendering performed by the angular template.
+                    // Therefore we remove it immediately and also move the original item back to the source list.
+                    // (event handler may be attached to the original item and not its clone, therefore keeping
+                    // the original dom node, circumvents side effects )
+                    this.renderer.removeChild(event.item.parentNode, event.item)
+                    this.renderer.insertBefore(event.clone.parentNode, event.item, event.clone)
+                    this.renderer.removeChild(event.clone.parentNode, event.clone)
+                } else {
+                    this.service.transfer?.(bindings.extractFromEvery(event.oldIndex!))
+                }
+
+                this.service.transfer = undefined
+            }
+
+            this.proxyEvent('onRemove', event)
+        },
+        onUpdate: (event: SortableEvent) => {
+            const bindings = this.bindings()
+
+            bindings.injectIntoEvery(event.newIndex!, bindings.extractFromEvery(event.oldIndex!))
+            this.proxyEvent('onUpdate', event)
+        }
+    }))
+
+    private readonly options = computed(() => ({ ...this.optionsWithoutEvents(), ...this.overridenOptions() }))
 
     /** @internal */
     ngAfterViewInit() {
@@ -75,7 +130,7 @@ export class SortablejsDirective<T> implements OnChanges, OnDestroy, AfterViewIn
             Object.keys(currentOptions).forEach(optionName => {
                 if (currentOptions[optionName as keyof Options] !== previousOptions[optionName as keyof Options]) {
                     // use low-level option setter
-                    this.sortableInstance?.option(optionName as keyof Options, this.options[optionName as keyof Options])
+                    this.sortableInstance?.option(optionName as keyof Options, this.options()[optionName as keyof Options])
                 }
             })
         }
@@ -94,35 +149,17 @@ export class SortablejsDirective<T> implements OnChanges, OnDestroy, AfterViewIn
 
         setTimeout(() => {
             if (typeof window != 'undefined') {
-                this.sortableInstance = Sortable.create(container, this.options)
+                this.sortableInstance = Sortable.create(container, this.options())
                 this.init.emit(this.sortableInstance)
             }
         }, 0)
     }
 
-    private getBindings(): SortablejsBindings<T> {
-        const data = this.data()
-        if (!data) {
-            return new SortablejsBindings([])
-        } else if (data instanceof SortablejsBindings) {
-            return data
-        } else {
-            return new SortablejsBindings([data])
-        }
-    }
-
-    private get options() {
-        return { ...this.optionsWithoutEvents, ...this.overridenOptions }
-    }
-
-    private get optionsWithoutEvents() {
-        return { ...(this.globalConfig || {}), ...(this.config() || {}) }
-    }
-
     private proxyEvent(eventName: string, ...params: any[]) {
         this.zone.run(() => { // re-entering zone, see https://github.com/SortableJS/angular-sortablejs/issues/110#issuecomment-408874600
-            if (this.optionsWithoutEvents?.[eventName as keyof typeof this.optionsWithoutEvents]) {
-                (this.optionsWithoutEvents[eventName as keyof typeof this.optionsWithoutEvents] as (...params: any[]) => void)(...params)
+            const opts = this.optionsWithoutEvents()
+            if (opts?.[eventName as keyof typeof opts]) {
+                (opts[eventName as keyof typeof opts] as (...params: any[]) => void)(...params)
             }
         })
     }
@@ -137,54 +174,6 @@ export class SortablejsDirective<T> implements OnChanges, OnDestroy, AfterViewIn
     private clone(item: T): T {
         // by default pass the item through, no cloning performed
         return (this.cloneFunction() || (subitem => subitem))(item)
-    }
-
-    private get overridenOptions(): Options {
-        // always intercept standard events but act only in case items are set (bindingEnabled)
-        // allows to forget about tracking this.items changes
-        return {
-            onAdd: (event: SortableEvent) => {
-                this.service.transfer = (items: any[]) => {
-                    this.getBindings().injectIntoEvery(event.newIndex!, items)
-                    this.proxyEvent('onAdd', event)
-                }
-
-                this.proxyEvent('onAddOriginal', event)
-            },
-            onRemove: (event: SortableEvent) => {
-                const bindings = this.getBindings()
-
-                if (bindings.provided) {
-                    if (this.isCloning) {
-                        this.service.transfer?.(bindings.getFromEvery(event.oldIndex!).map(item => this.clone(item)))
-
-                        // great thanks to https://github.com/tauu
-                        // event.item is the original item from the source list which is moved to the target list
-                        // event.clone is a clone of the original item and will be added to source list
-                        // If bindings are provided, adding the item dom element to the target list causes artifacts
-                        // as it interferes with the rendering performed by the angular template.
-                        // Therefore we remove it immediately and also move the original item back to the source list.
-                        // (event handler may be attached to the original item and not its clone, therefore keeping
-                        // the original dom node, circumvents side effects )
-                        this.renderer.removeChild(event.item.parentNode, event.item)
-                        this.renderer.insertBefore(event.clone.parentNode, event.item, event.clone)
-                        this.renderer.removeChild(event.clone.parentNode, event.clone)
-                    } else {
-                        this.service.transfer?.(bindings.extractFromEvery(event.oldIndex!))
-                    }
-
-                    this.service.transfer = undefined
-                }
-
-                this.proxyEvent('onRemove', event)
-            },
-            onUpdate: (event: SortableEvent) => {
-                const bindings = this.getBindings()
-
-                bindings.injectIntoEvery(event.newIndex!, bindings.extractFromEvery(event.oldIndex!))
-                this.proxyEvent('onUpdate', event)
-            }
-        }
     }
 
 }
